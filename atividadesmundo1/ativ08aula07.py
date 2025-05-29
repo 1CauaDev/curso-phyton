e832c16aeca41e7d5516fd8e4508a2a48f5cb0c7
Cauã <rochacaua143@gmail.com>
2025-05-29 19:52:12 -0300

    atividade da aula 07

---
+++ atividadesmundo1/ativ08aula07.py
@@ -0,0 +1,6 @@
+l = float(input(" ME INFORME A LARGURA DA PAREDE:"))
+A = float(input(" ME INFORME A ALTURA DA PARADE : "))
+multialtlarg = float(l)*float(A)
+tinta = multialtlarg/2
+print('a largura é {} , e a altura é {}'.format(l,A))
+print("o total de tinta usado sera {}".format(tinta))

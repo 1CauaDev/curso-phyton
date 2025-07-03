eb5307fbeb6918c6065ef0a29425bc2f0a7baa23
Cauã <rochacaua143@gmail.com>
2025-07-02 21:23:38 -0300

    salario dos funcionarios

---
+++ atividadesmundo1/ativ10aula07.py
@@ -0,0 +1,6 @@
+salario=int(input("ME INFORME SEU SALARIO ATUAL:"))
+novosalario=(salario*15)/100
+totalsalario=novosalario + salario
+print("seu salario atual ficou R$",totalsalario)
+print("seu salario que era R$",salario)
+print("teve o aumento de R$",novosalario)

9bf5232a43c1e3f60dcc8caad297d78d50d040a0
Cauã <rochacaua143@gmail.com>
2025-08-05 22:03:18 -0300

    condiçoes

---
+++ Aulamodulo.10/exercicio.py
@@ -0,0 +1,9 @@
+nota1 = float(input("digite a primeira nota:"))
+nota2= float(input("Digite o segunda nota :"))
+media = (nota1 + nota2)/2
+print("A media é {}".format(media))
+if media >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
+print("FIM DO PROGRAMA!")

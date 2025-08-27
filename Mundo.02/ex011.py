fec3d654692d724d8bf2773f6ae3d98f961d1f4c
Cauã <rochacaua143@gmail.com>
2025-08-26 23:09:11 -0300

    tabuada ultilizando for

---
+++ Mundo.02/ex011.py
@@ -0,0 +1,5 @@
+for num in range(1,500):
+    if num % 2 == 1 and num % 3 == 0:
+        print(num)
+
+print(" FIM DO PROGRAMA")
---
+++ Mundo.02/ex012.py
@@ -0,0 +1,4 @@
+tabuada = int(input("Digite um numero para ver sua tabuada:"))
+for num in range(1,11):
+    print("{} x {} = {}".format(tabuada,num,tabuada*num))
+print("FIM DA TABUADA!")

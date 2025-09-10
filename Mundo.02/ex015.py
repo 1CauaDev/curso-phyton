84d0b067b85619cf8aac6463ce6c8a4d4fa60dd5
Cauã <rochacaua143@gmail.com>
2025-09-10 17:24:01 -0300

    notificaçao no windows

---
+++ Mundo.02/ex015.py
@@ -0,0 +1,13 @@
+
+print("BEM VINDO A MATEMATICA INTELIGENTE!")
+print("[1] SOMAR")
+print("[2] SUBTRAIR")
+print("[3] Multiplicar")
+print("[4] Sair")
+opcao = int(input("Escolha uma opcao:"))
+while opcao !=5:
+    if opcao == 1 :
+        def funcao_opcao_1(a,b):
+            resultado = a + b
+            return resultado
+        
